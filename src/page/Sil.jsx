4674b4ce1cc37991be23be2c3f2243681e6a593b
Yoselin <yoselinporras034@gmail.com>
2026-02-04 17:14:45 -0600

    inicio de pantallas

---
+++ src/page/Sil.jsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+function Sil(){
+    return(
+        <>
+            <h1>Sil</h1>
+            <Link to="/">Home</Link>
+        </>
+    )
+}
+
+export default Sil;
